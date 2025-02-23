fe079c95f75a4afa154ed2af15967fcbca489ea2
andy-thieu <thieuandyhuy@gmail.com>
2025-02-23 16:49:17 +0100

    feat: db setup

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,9 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
 import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
+    integer,
+    pgTableCreator, serial, text,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -18,19 +14,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `wolfwer_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+export const users = createTable(
+  "users",
+    {
+        id: serial('id').primaryKey(),
+        username: text('username').notNull(),
+        wins: integer('wins').notNull().default(0),
+        losses: integer('losses').notNull().default(0),
+    }
 );
