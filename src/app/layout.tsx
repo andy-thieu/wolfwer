45bb9e0cf2b14f4f07fbe873d6a9cc38f95de668
endidii <thieuandyhuy@gmail.com>
2025-02-23 23:11:29 +0100

    fix: suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Wolfwer",
@@ -13,8 +14,10 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
-    </html>
+    <Suspense fallback={<div>Loading...</div>}>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>{children}</body>
+      </html>
+    </Suspense>
   );
 }
